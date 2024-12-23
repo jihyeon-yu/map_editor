705a204925e22df88dabbbdf8622762113a79e89
jihyeon-yu <fbwlgus6@naver.com>
2024-12-23 09:33:09 +0900

    Feat: delete all

--- sdk/build/intermediates/cxx/Debug/1u124j44/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\fbwlg\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\fbwlg\\OneDrive\\Desktop\\map\\OnDeviceApp\\20241222_28\\OnDevice_20241222_28_seongwoong\\sdk\\.cxx\\Debug\\1u124j44\\arm64-v8a" ^
-  opencv_jni_shared
